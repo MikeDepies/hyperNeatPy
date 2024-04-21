a6e300a573c8e2fdddce95ba8779eed378f2eefd
Michael Depies <depiesms22@gmail.com>
2024-04-20 21:53:24 -0500

    changes

--- src/mario.py
+++ src/mario.py
@@ -202,7 +202,7 @@ if __name__ == "__main__":
 
         processes = []
 
-        for i in range(10):
+        for i in range(20):
 
             p = Process(target=simulation, args=(queue, i < 1))
             p.start()
