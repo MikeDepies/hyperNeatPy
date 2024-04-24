837dfc0adad659d2a5d6438377ae57c8c91dff13
Mike Depies <depiesms22@gmail.com>
2024-04-24 17:52:19 -0500

    'vote' based output

--- src/mario.py
+++ src/mario.py
@@ -47,10 +47,10 @@ def simulate_environment(
     hidden_coords = [
         [
             (x, y, z)
-            for x in np.linspace(-1, 1, round(width ))
-            for y in np.linspace(-1, 1, round(height))
+            for x in np.linspace(-1, 1, round(width / 2))
+            for y in np.linspace(-1, 1, round(height / 2))
         ]
-        for z in np.linspace(-0.5, 0.5, round(3))
+        for z in np.linspace(-0.5, 0.5, round(5))
     ]
     # for z in np.linspace(-.5, .5, round(3))
     # data_dim = 3
