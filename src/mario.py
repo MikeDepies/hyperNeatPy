27b497158a145eb923e2cff011d86e1ee41c185a
Mike Depies <depiesms22@gmail.com>
2024-05-09 13:48:02 -0500

    increased layer size

--- src/mario.py
+++ src/mario.py
@@ -314,8 +314,8 @@ if __name__ == "__main__":
     hidden_coords = [
         [
             (x, y, z)
-            for x in np.linspace(-1, 1, 8)
-            for y in np.linspace(-1, 1, 8)
+            for x in np.linspace(-1, 1, 16)
+            for y in np.linspace(-1, 1, 16)
         ]
         for z in np.linspace(-0.9, 0.9, round(30))
     ]
