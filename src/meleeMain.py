b5b0482eb72291df5c84d2c25cc41ad35ff01863
Mike Depies <depiesms22@gmail.com>
2024-05-27 16:51:12 -0500

    melee

--- src/meleeMain.py
+++ src/meleeMain.py
@@ -991,7 +991,7 @@ def main():
     # for y in np.linspace(-1, 1, 1)
     hidden_coords = [
         [(y, x, z) for y in np.linspace(-1, 1, 12) for x in np.linspace(-1, 1, 16)]
-        for z in np.linspace(-0.9, 0.0, round(8))
+        for z in np.linspace(-0.9, 0.0, round(3))
     ]
     output_width = 5
     output_height = 1
