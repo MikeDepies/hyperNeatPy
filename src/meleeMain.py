73329215ee7b5da9918672ebc8ad046b56eb5be8
Mike Depies <depiesms22@gmail.com>
2024-06-02 17:31:10 -0500

    no threshold for weight

--- src/meleeMain.py
+++ src/meleeMain.py
@@ -561,7 +561,7 @@ class MeleeSimulation:
         self.melee_config = melee_config
         self.controller_helper = ControllerHelper()
         self.use_action_coords = use_action_coords
-        self.action_tracker = ActionTracker(30)
+        self.action_tracker = ActionTracker(20)
 
     def set_config(self, melee_config: MeleeConfiguration):
         self.melee_config = melee_config
