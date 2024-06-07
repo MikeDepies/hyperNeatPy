d80a797fa72fbcd4041deb2ded8fa16d8bd5fe4c
Mike Depies <depiesms22@gmail.com>
2024-06-06 22:58:52 -0500

    update to substrate

--- src/meleeMain.py
+++ src/meleeMain.py
@@ -1280,7 +1280,7 @@ def main():
     # for y in np.linspace(-1, 1, 1)
     hidden_coords = [
         [(y, x, 0) for y in np.linspace(-1, 1, 8) for x in np.linspace(-1, 1, 8)]
-        for z in np.linspace(0.0, 0.6, round(3))
+        for z in np.linspace(0.0, 0.6, round(10))
     ]
     output_width = 5
     output_height = 1
