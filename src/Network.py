f0acc384372b7e73bfcf8dd5b9730b8b94c6accd
Mike Depies <depiesms22@gmail.com>
2024-06-19 13:11:43 -0500

    norms

--- src/Network.py
+++ src/Network.py
@@ -774,7 +774,7 @@ class TaskNetwork2(torch.nn.Module):
             self.hidden_output_weights,
             *self.hidden_bias_weights,
             self.output_bias_weights,
-            *self.hidden_recurrent_weights,
+            # *self.hidden_recurrent_weights,
         ]:
             l2_norm += torch.norm(weight_matrix, p=2).item()
         return l2_norm
@@ -787,7 +787,7 @@ class TaskNetwork2(torch.nn.Module):
             self.hidden_output_weights,
             *self.hidden_bias_weights,
             self.output_bias_weights,
-            *self.hidden_recurrent_weights,
+            # *self.hidden_recurrent_weights,
         ]:
             l1_norm += torch.norm(weight_matrix, p=1).item()
         return l1_norm
@@ -811,8 +811,8 @@ class TaskNetwork2(torch.nn.Module):
         self.hidden_activations[0] = (
             torch.matmul(inputs, self.input_hidden_weights)
             + self.hidden_bias_weights[0]
-        )   + torch.matmul(self.hidden_activations[0], self.hidden_recurrent_weights[0])
-        self.hidden_activations[0] = torch.relu(
+        )   #+ torch.matmul(self.hidden_activations[0], self.hidden_recurrent_weights[0])
+        self.hidden_activations[0] = torch.sigmoid(
             self.hidden_activations[0]
         )  # Activation function
         for i in range(len(self.substrate.hidden_coords) - 1):
@@ -826,11 +826,11 @@ class TaskNetwork2(torch.nn.Module):
             # print(f"{i} == {len(self.substrate.hidden_coords) - 1}")
             # if (i + 2) == len(self.substrate.hidden_coords) - 1:
                 # print("TESTTTTTTTTTTTTTTT")
-            self.hidden_activations[i + 1] += torch.matmul(
-                self.hidden_activations[i + 1], self.hidden_recurrent_weights[i + 1]
-            )
+            # self.hidden_activations[i + 1] += torch.matmul(
+            #     self.hidden_activations[i + 1], self.hidden_recurrent_weights[i + 1]
+            # )
             
-            self.hidden_activations[i + 1] = torch.relu(
+            self.hidden_activations[i + 1] = torch.sigmoid(
                 self.hidden_activations[i + 1]
             )  # Activation function
         # self.hidden_activations = hidden_activations
