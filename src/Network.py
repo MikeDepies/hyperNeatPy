b8c0b587d2dd904bbbb1e0d4331c47926867a1b5
Mike Depies <depiesms22@gmail.com>
2024-07-11 09:57:12 -0500

    rnn change

--- src/Network.py
+++ src/Network.py
@@ -775,7 +775,7 @@ class TaskNetwork2(torch.nn.Module):
             self.hidden_output_weights,
             *self.hidden_bias_weights,
             self.output_bias_weights,
-            *self.hidden_recurrent_weights,
+            # *self.hidden_recurrent_weights,
         ]:
             l2_norm += torch.norm(weight_matrix, p=2).item()
         return l2_norm
@@ -812,8 +812,8 @@ class TaskNetwork2(torch.nn.Module):
         self.hidden_activations[0] = (
             torch.matmul(inputs, self.input_hidden_weights)
             + self.hidden_bias_weights[0]
-        ) + torch.matmul(self.hidden_activations[0], self.hidden_recurrent_weights[0])
-        self.hidden_activations[0] = torch.sigmoid(
+        )# + torch.matmul(self.hidden_activations[0], self.hidden_recurrent_weights[0])
+        self.hidden_activations[0] = torch.relu(
             self.hidden_activations[0]
         )  # Activation function
         for i in range(len(self.substrate.hidden_coords) - 1):
@@ -825,11 +825,11 @@ class TaskNetwork2(torch.nn.Module):
             # print(f"{i} == {len(self.substrate.hidden_coords) - 1}")
             # if (i + 2) == len(self.substrate.hidden_coords) - 1:
             # print("TESTTTTTTTTTTTTTTT")
-            self.hidden_activations[i + 1] += torch.matmul(
-                self.hidden_activations[i + 1], self.hidden_recurrent_weights[i + 1]
-            )
+            # self.hidden_activations[i + 1] += torch.matmul(
+            #     self.hidden_activations[i + 1], self.hidden_recurrent_weights[i + 1]
+            # )
 
-            self.hidden_activations[i + 1] = torch.sigmoid(
+            self.hidden_activations[i + 1] = torch.relu(
                 self.hidden_activations[i + 1]
             )  # Activation function
         # self.hidden_activations = hidden_activations
