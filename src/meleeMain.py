cc6753ddddf475bad9ddba2d64b6eaa74b0ac028
Mike Depies <depiesms22@gmail.com>
2024-07-24 18:50:38 -0500

    dynamic thresh for all locations fix

--- src/meleeMain.py
+++ src/meleeMain.py
@@ -1422,8 +1422,8 @@ def main():
     ]
     # for y in np.linspace(-1, 1, 1)
     hidden_coords = [
-        [(y, x, z) for y in np.linspace(-1, 1, 8-i) for x in np.linspace(-1, 1, 8-i)]
-        for i, z in enumerate(np.linspace(-0.6, 0.6, round(5)))
+        [(y, x, 0) for y in np.linspace(-1, 1, 8) for x in np.linspace(-1, 1, 8)]
+        # for i, z in enumerate(np.linspace(-0.6, 0.6, round(5)))
     ]
     output_width = 5
     output_height = 1
